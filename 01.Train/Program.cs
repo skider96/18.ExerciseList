fba39311acfb9155ec4d0dbe89b35933ac7be9d0
skider96 <danailqkiq@gmail.com>
2024-04-08 20:23:24 +0300

    Solved 1 problem in 18.ExcerciseList

--- 01.Train/Program.cs
+++ 01.Train/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace _01.Train
+﻿using System.Threading.Channels;
+
+namespace _01.Train
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<int> wagonsList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int maxWagonCapacity = int.Parse(Console.ReadLine());
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] act = command.Split().ToArray();
+                int number = 0;
+                if (act.Length == 2 && act[0] == "Add" && int.TryParse(act[1], out number))
+                {
+                    wagonsList.Add(number);
+                }
+                else if (act.Length == 1 && int.TryParse(act[0], out number))
+                {
+                    int lastEmptyWagon = wagonsList.FindIndex(w => maxWagonCapacity - w >= int.Parse(act[0]));
+
+                    if (lastEmptyWagon != -1) wagonsList[lastEmptyWagon] += number;
+                }
+            }
+
+            wagonsList.ForEach(w => Console.Write(w + " "));
         }
     }
 }
--- 02.ChangeList/Program.cs
+++ 02.ChangeList/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            
+            List<int> integers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] act = command.Split().ToArray();
+                int number = 0;
+                if (act.Length == 2 && act[0] == "Delete" && int.TryParse(act[1], out number))
+                {
+                    //    integers.Where(x => x == act[1])
+                }
+            }
+
+            //TO BE SOLVED
         }
     }
 }
