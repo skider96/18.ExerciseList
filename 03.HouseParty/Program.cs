d9a8a47dff1fc2d6976f5be5fa448a754cca7597
skider96 <danailqkiq@gmail.com>
2024-04-08 23:24:37 +0300

    Solved 1 problem in 18.ExcerciseList

--- 03.HouseParty/Program.cs
+++ 03.HouseParty/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace _03.HouseParty
+﻿using System.Threading.Channels;
+
+namespace _03.HouseParty
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            List<string> guestList = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+                if (input.Length == 3)
+                {
+                    if (!guestList.Contains(input[0]))
+                    {
+                        guestList.Add(input[0]);
+                        continue;
+                    }
+
+                    Console.WriteLine($"{input[0]} is already in the list!");
+                }
+                else if (input.Length == 4) 
+                {
+                    if (guestList.Contains(input[0]))
+                    {
+                        guestList.Remove(input[0]);
+                        continue;
+                    }
+
+                    Console.WriteLine($"{input[0]} is not in the list!");
+                }
+            }
+              
+            guestList.ForEach(g => Console.WriteLine(g));
         }
     }
 }
