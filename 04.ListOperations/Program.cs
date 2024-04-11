ca68cb315196bd568d545f7cd438d2b13250ca00
skider96 <danailqkiq@gmail.com>
2024-04-11 22:07:09 +0300

    Solved 1 problem in 18.ExcerciseList

--- 04.ListOperations/Program.cs
+++ 04.ListOperations/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace _04.ListOperations
+﻿using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
+namespace _04.ListOperations
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbersList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                bool isValidIndex = false;
+                string[] operations = command.Split().ToArray();
+
+                if (operations[0] == "Add" && operations.Length == 2)
+                {
+                    int givenNumber = int.Parse(operations[1]);
+                    numbersList.Add(givenNumber);
+                }
+                else if (operations[0] == "Insert" && operations.Length == 3)
+                {
+                    int givenNumber = int.Parse(operations[1]);
+                    int index = int.Parse(operations[2]);
+
+                    if (index >= numbersList.Count || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+
+                    numbersList.Insert(index, givenNumber);
+                }
+                else if (operations[0] == "Remove" && operations.Length == 2)
+                {
+                    int index = int.Parse(operations[1]);
+
+                    if (index >= numbersList.Count || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+
+                    numbersList.RemoveAt(index);
+                }
+                else if (operations[0] == "Shift" && operations[1] == "left" && operations.Length == 3)
+                {
+                    for (int i = 0; i < int.Parse(operations[2]); i++)
+                    {
+                        int firstNumuber = numbersList[0];
+
+                        int chosenIndex = 0;
+                        numbersList.RemoveAt(chosenIndex);
+
+                        numbersList.Add(firstNumuber);
+                    }
+                }
+                else if (operations[0] == "Shift" && operations[1] == "right" && operations.Length == 3)
+                {
+                    for (int i = 0; i < int.Parse(operations[2]); i++)
+                    {
+                        int lastNumber = numbersList[^1];
+
+                        numbersList.RemoveAt(numbersList.Count - 1);
+                        numbersList.Insert(0, lastNumber);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid index");
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbersList));
         }
     }
 }
